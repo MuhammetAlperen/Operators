73a4c4654a114ddb477d856a7912fd0c7208e769
example <yesildag4123@gmail.com>
2022-07-28 09:18:29 +0300

    operators

---
+++ Birleştirme/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birleştirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            Console.WriteLine("Adınızı Giriniz");
+            string FırstName = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Soyadınızı Giriniz");
+            string LastName = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("İsminiz :" + FırstName + LastName);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+    
+
